5588d7240ae24cf41e7cb7c805f337671af2e339
Aaron Parry <aaronparry@aarons-air.mynetworksettings.com>
2025-04-25 07:07:28 -0400

    add body handler

--- src/app/api/sms/route.ts
+++ src/app/api/sms/route.ts
@@ -1,23 +1,41 @@
+import { getUserByPhoneNumber } from '@/server/db/postgres/users';
 import { NextRequest, NextResponse } from 'next/server';
 import twilio from 'twilio';
 
 export async function POST(req: NextRequest) {
   try {
+    // Create a TwiML response
+    const MessagingResponse = twilio.twiml.MessagingResponse;
+    const twiml = new MessagingResponse();
+    
     // Parse the form data from the request
     const formData = await req.formData();
     const body = Object.fromEntries(formData);
     
     // Extract message content
     const incomingMessage = body.Body as string || '';
-    console.log(body);
+
+    const from = body.From as string || '';
+
+    const user = await getUserByPhoneNumber(from);
+
+    if (!user) {
+        twiml.message(
+            `Sign up now! https://www.gymtext.co/`
+          );      
+          return new NextResponse(twiml.toString(), {
+            status: 200,
+            headers: {
+              'Content-Type': 'text/xml',
+            },
+          });
+          }
     
-    // Create a TwiML response
-    const MessagingResponse = twilio.twiml.MessagingResponse;
-    const twiml = new MessagingResponse();
+    
     
     // Logic based on the message content
     twiml.message(
-      `You said: ${incomingMessage}`
+      `${user.name} said: ${incomingMessage}`
     );
     
     // Return the TwiML response with the appropriate content type
